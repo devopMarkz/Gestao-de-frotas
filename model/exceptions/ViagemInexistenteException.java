2ff24b967403f45be5527aa9ff69500d6658e5dc
devopMarkz <marcosdev2002@gmail.com>
2024-09-25 09:47:52 -0300

    Criando exceção para o caso do usuário escolher um ID de viagem inexistente no sistema

---
+++ model/exceptions/ViagemInexistenteException.java
@@ -0,0 +1,10 @@
+package model.exceptions;
+
+@SuppressWarnings("serial")
+public class ViagemInexistenteException extends Exception {
+	
+	public ViagemInexistenteException(String msg) {
+		super(msg);
+	}
+
+}
