de7dd3ef5124f0de662d0b6d81c6f7c69ae052ec
devopMarkz <marcosdev2002@gmail.com>
2024-09-25 14:24:32 -0300

    Criando exceções para Veiculos indisponíveis ou inexistentes e Motoristas indisponíveis

---
+++ model/exceptions/VeiculoIndisponivelException.java
@@ -0,0 +1,10 @@
+package model.exceptions;
+
+@SuppressWarnings("serial")
+public class VeiculoIndisponivelException extends Exception {
+	
+	public VeiculoIndisponivelException(String msg) {
+		super(msg);
+	}
+
+}
