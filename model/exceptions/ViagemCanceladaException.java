f0c4192c7a203f0adafbee7a2b2f527fb33e09a3
devopMarkz <marcosdev2002@gmail.com>
2024-09-25 20:21:56 -0300

    Criando classes para disparar exceções

---
+++ model/exceptions/ViagemCanceladaException.java
@@ -0,0 +1,10 @@
+package model.exceptions;
+
+@SuppressWarnings("serial")
+public class ViagemCanceladaException extends Exception {
+	
+	public ViagemCanceladaException(String msg) {
+		super(msg);
+	}
+
+}
---
+++ model/exceptions/ViagemConcluidaException.java
@@ -0,0 +1,10 @@
+package model.exceptions;
+
+@SuppressWarnings("serial")
+public class ViagemConcluidaException extends Exception {
+	
+	public ViagemConcluidaException(String msg) {
+		super(msg);
+	}
+
+}
