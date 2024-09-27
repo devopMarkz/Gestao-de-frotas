f516bc4fa0823d30479981a6a3823e4596bd0470
devopMarkz <marcosdev2002@gmail.com>
2024-09-27 17:00:39 -0300

    Versão 1.0

--- application/Program.java
+++ application/Program.java
@@ -40,31 +40,3 @@ public class Program {
 	}
 
 }
-
-/* SUCESSO - ARQUIVO SALVANDO EM REGISTRODEVIAGENS.TXT
- * Viagem viagem = new Viagem(1, new Onibus("ptj1d34", "Volvo", "Mercedes", 2022, 2000.00, false, 200.00, CategoriaVeiculo.ONIBUS, Combustivel.DIESEL, 32), new Motorista("Marcos", "1378322726", CategoriaCNH.C, LocalDate.parse("2992-09-25"), true), LocalDate.now());
-
-try (BufferedWriter bw = new BufferedWriter(new FileWriter(Frota.registroDeViagens, true))){
-	bw.write(viagem.imprimirNoArquivo());
-	bw.newLine();
-} catch (Exception e) {
-	System.out.println("Error: " + e.getMessage());
-}*/
-
-/* SUCESSO - ARQUIVO SALVANDO EM REGISTRODEVEICULOS.TXT
- * Veiculo veiculo = new Onibus("ptj1d34", "Volvo", "Mercedes", 2022, 2000.00, false, 200.00, CategoriaVeiculo.ONIBUS, Combustivel.DIESEL, 32);
-
-try (BufferedWriter bw = new BufferedWriter(new FileWriter(Frota.registroDeVeiculos))){
-	bw.write(veiculo.imprimirNoArquivo());
-} catch (Exception e) {
-	System.out.println("Error: " + e.getMessage());
-}*/
-
-/* SUCESSO - ARQUIVO SALVANDO EM REGISTRODEFUNCIONARIOS.TXT
- * Motorista motorista = new Motorista("Marcos", "1378322726", CategoriaCNH.C, LocalDate.parse("2992-09-25"), true);
-
-try (BufferedWriter bw = new BufferedWriter(new FileWriter(Frota.registroDeMotoristas))){
-	bw.write(motorista.imprimirNoArquivo());
-} catch (Exception e) {
-	System.out.println("Error: " + e.getMessage());
-}*/
