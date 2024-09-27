ea28a28f5a1f911c8a26a95be30ac57e81980d6c
devopMarkz <marcosdev2002@gmail.com>
2024-09-26 21:44:51 -0300

    Criando exceção para o caso do usuário escolher uma opção de entrada inválida do sistema

---
+++ model/exceptions/OpcaoInvalidaException.java
@@ -0,0 +1,10 @@
+package model.exceptions;
+
+@SuppressWarnings("serial")
+public class OpcaoInvalidaException extends Exception {
+	
+	public OpcaoInvalidaException(String msg) {
+		super(msg);
+	}
+
+}
